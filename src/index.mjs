f3df4621b4e6fed9933b421de72803af935c1ab9
Jascha <jascha@jaeh.at>
2019-05-20 10:31:39 +0200

    only export is as default

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,14 @@
 export const is = (e, type) => (typeof is[type] === 'function' ? is[type](e) : typeof e === type)
 
-export const number = is.number = e => e === +e
-export const integer = is.integer = e => e === +e && e === (e | 0)
-export const float = is.float = e => e === +e
+is.number = e => e === +e
+is.integer = e => e === +e && e === (e | 0)
+is.float = e => e === +e
 
-export const array = is.array = e => Array.isArray(e)
-export const regexp = is.regexp = e => e instanceof RegExp
-export const date = is.date = e => e instanceof Date
-export const error = is.error = e => e instanceof Error
-export const nil = is.null = is.nil = e => e === null
-export const promise = is.promise = e => e instanceof Promise
+is.array = e => Array.isArray(e)
+is.regexp = e => e instanceof RegExp
+is.date = e => e instanceof Date
+is.error = e => e instanceof Error
+is.null = is.nil = e => e === null
+is.promise = e => e instanceof Promise
 
 export default is
